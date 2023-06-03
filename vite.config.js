95656ad9b81fa86949389be67339cd4f202f409a
unknown <zjnkill18@gmail.com>
2023-06-04 00:44:44 +0700

    1111

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,20 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-  },
-})
+  plugins: [vue()],
+  build: {
+      lib: {
+        entry: resolve('./resources/js/field.js'),
+        name: 'FroalaField',
+        fileName: 'froala-field'
+      },
+      rollupOptions: {
+        external: ['vue', 'laravel-nova', 'froala-editor'],
+        output: {
+          globals: {
+            vue: 'Vue'
+          }
+        }
+      }
+  }
+});
